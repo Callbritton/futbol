acf65f2fd5a8f69dc8dd97a720ef883cfbecd545
Logan Riffell <lkriffell@gmail.com>
2020-07-20 17:30:04 -0600

    Update highest average scoring visitor method, skipping current test to test helper

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -8,6 +8,9 @@ class LeagueStatistics
   end
 
   def highest_average_scoring_visitor
-
+    @game_hash["away_team_id"].each_with_index.group_by(&:first).inject({}) do |result, (away_id, indeces)|
+      next result if indeces.length == 1
+      result.merge away_id => indeces.map {|pair| pair[1]}
+    end
   end
 end
