634ba9b8d4ab75f5fbea442e4894b379f8b25730
Priya Power <priyajag72@gmail.com>
2020-07-20 11:51:13 -0600

    Add minitest syntax to league statistics test

---
+++ test/league_statistics_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/league_statistics"
+require "./lib/game"
+require 'csv'
+require 'pry'
+
+class LeagueStatisticsTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
