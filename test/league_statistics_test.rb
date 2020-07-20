f6015ca672609a52b33065a62542268a8e5e9680
Priya Power <priyajag72@gmail.com>
2020-07-20 12:03:14 -0600

    Add setup method to League Statistics Test with csv paths, location hash, data instantiations and data hash

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -7,9 +7,28 @@ require 'pry'
 
 class LeagueStatisticsTest < Minitest::Test
 
+  def setup
+    game_path = './data/dummy_file_games.csv'
+    team_path = './data/dummy_file_teams.csv'
+    game_teams_path = './data/dummy_file_game_teams.csv'
+
+    @locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @game_stats = Game.new()
+    @team_stats = Game.new()
+    @league_statistics = LeagueStatistics.new
+    game_array = CSV.read(@locations[:games])
+    team_array = CSV.read(@locations[:teams])
+    @game_stats.create_stat_hash_keys(game_array)
+    @team_stats.create_stat_hash_keys(team_array)
+  end
+
   def test_it_exists
-    league_statistics = LeagueStatistics.new
-    assert_instance_of LeagueStatistics, league_statistics
+    assert_instance_of LeagueStatistics, @league_statistics
   end
 
   #def test_it_has_attributes
