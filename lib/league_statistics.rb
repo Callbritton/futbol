38f4180fd74eea283c0e9219922dbcb7db66ee4f
Logan Riffell <lkriffell@gmail.com>
2020-07-21 12:27:24 -0600

    Add game_data to create instances of each line of data

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -1,32 +1,44 @@
 class LeagueStatistics
 
-  attr_reader :game_hash, :team_hash
+  attr_reader :all_data
 
-  def initialize(game_hash, team_hash)
-    @game_hash = game_hash
-    @team_hash = team_hash
+  def initialize(all_data)
+    @all_data = all_data
   end
 
   # def highest_average_scoring_visitor
   # end
 
-  def pull_goals_by_team_id(info, key)
-    hash_creation_by_index(info, key).each do |team_id, value|
-      goals = value.reduce({}) do |goals, index|
-        goal = @game_hash["away_goals"][index]
-        goals.merge(team_id => goal)
+  # def pull_goals_by_team_id(info, key)
+  #   hash_creation_by_index(info, key).each do |team_id, value|
+  #     goals = value.reduce({}) do |goals, index|
+  #       goal = @game_hash["away_goals"][index]
+  #       goals.merge(team_id => goal)
+  #     end
+  #     require "pry"; binding.pry
+  #     goals
+  #   end
+  # end
+  #
+  # def hash_creation_by_index(info, key)
+  #   any_id_hash_by_index = info[key].each_with_index.group_by(&:first).inject({}) do |result, (away_id, indeces)|
+  #     next result if indeces.length == 1
+  #     result.merge away_id => indeces.map {|pair| pair[1]}
+  #   end
+  #   any_id_hash_by_index
+  # end
+
+  def best_offense
+    team_id = 3
+    total_goals_by_id = []
+    while team_id < @all_data.size
+      x = @all_data.find_all do |game|
+        game.away_team_id == team_id.to_s || game.home_team_id == team_id.to_s
       end
-      require "pry"; binding.pry
-      goals
+        # total_goals_by_id << game.away_goals
+        # total_goals_by_id << game.home_goals
+    team_id += 1
+    require "pry"; binding.pry
     end
   end
-
-  def hash_creation_by_index(info, key)
-    any_id_hash_by_index = info[key].each_with_index.group_by(&:first).inject({}) do |result, (away_id, indeces)|
-      next result if indeces.length == 1
-      result.merge away_id => indeces.map {|pair| pair[1]}
-    end
-    any_id_hash_by_index
-  end
-
 end
--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/league_statistics"
-require "./lib/game"
+require "./lib/game_data"
 require 'csv'
 require 'pry'
 
@@ -17,42 +17,25 @@ class LeagueStatisticsTest < Minitest::Test
       teams: team_path,
       game_teams: game_teams_path
     }
-
-    @game_stats = Game.new()
-    @team_stats = Game.new()
-    game_array = CSV.read(@locations[:games])
-    team_array = CSV.read(@locations[:teams])
-    @game_stats.create_stat_hash_keys(game_array)
-    @team_stats.create_stat_hash_keys(team_array)
-
-    @league_statistics = LeagueStatistics.new(@game_stats.stat_hash, @team_stats.stat_hash)
+    @table = CSV.parse(File.read('./data/dummy_file_games.csv'), headers: true)
+    line_index = 0
+    @all_games = []
+    @table.size.times do
+      game_data = GameData.new
+      game_data.create_attributes(@table, line_index)
+      @all_games << game_data
+      line_index += 1
+    end
+    @league_statistics = LeagueStatistics.new(@all_games)
   end
 
   def test_it_exists
     assert_instance_of LeagueStatistics, @league_statistics
   end
 
-  def test_it_has_attributes
-    skip
-    assert_equal [], @league_statistics.game_hash #dummy data call
-    assert_equal [], @league_statistics.team_hash #dummy data call
-  end
+  def test_best_offense
 
-  def test_highest_average_scoring_visitor
-    skip
-
-    assert_equal "team_name", @league_statistics.highest_average_scoring_visitor
-  end
-
-  def test_hash_creation_by_index
-    expected = {"3"=>[0, 1, 4]}
-
-    assert_equal expected, @league_statistics.hash_creation_by_index(@league_statistics.game_hash, "away_team_id").slice("3")
-  end
-
-  def test_pull_goals_by_team_id
-
-    assert_equal ["2", "2", "1"], @league_statistics.pull_goals_by_team_id(@league_statistics.game_hash, "away_team_id")
+    assert_equal "team_name", @league_statistics.best_offense
   end
 
 end
