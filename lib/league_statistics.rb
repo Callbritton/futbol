a5d06013c8d87e351d47b144aa37034a8e2577d6
Logan Riffell <lkriffell@gmail.com>
2020-07-20 19:09:30 -0600

    Add unfinished pull_goals_by_team method

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -10,6 +10,17 @@ class LeagueStatistics
   # def highest_average_scoring_visitor
   # end
 
+  def pull_goals_by_team_id(info, key)
+    hash_creation_by_index(info, key).each do |team_id, value|
+      goals = value.reduce({}) do |goals, index|
+        goal = @game_hash["away_goals"][index]
+        goals.merge(team_id => goal)
+      end
+      require "pry"; binding.pry
+      goals
+    end
+  end
+
   def hash_creation_by_index(info, key)
     any_id_hash_by_index = info[key].each_with_index.group_by(&:first).inject({}) do |result, (away_id, indeces)|
       next result if indeces.length == 1
@@ -18,7 +29,4 @@ class LeagueStatistics
     any_id_hash_by_index
   end
 
-  def pull_goals_by_index
-    
-  end
 end
