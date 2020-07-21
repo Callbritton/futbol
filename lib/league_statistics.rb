af39f867c7ad49fbba1cdc61d78a7e38af9a8d0e
Logan Riffell <lkriffell@gmail.com>
2020-07-20 18:23:10 -0600

    Add pull_goals_by_index method

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -7,10 +7,18 @@ class LeagueStatistics
     @team_hash = team_hash
   end
 
-  def highest_average_scoring_visitor
-    @game_hash["away_team_id"].each_with_index.group_by(&:first).inject({}) do |result, (away_id, indeces)|
+  # def highest_average_scoring_visitor
+  # end
+
+  def hash_creation_by_index(info, key)
+    any_id_hash_by_index = info[key].each_with_index.group_by(&:first).inject({}) do |result, (away_id, indeces)|
       next result if indeces.length == 1
       result.merge away_id => indeces.map {|pair| pair[1]}
     end
+    any_id_hash_by_index
+  end
+
+  def pull_goals_by_index
+    
   end
 end
