6b2b3e75b8fbdb650d61c99c771b062fac687fb8
Logan Riffell <lkriffell@gmail.com>
2020-07-24 20:58:15 -0600

    Add season_statistics class skeleton

---
+++ lib/season_statistics.rb
@@ -0,0 +1,34 @@
+require_relative "team_data"
+require_relative "game_team_data"
+require_relative "game_data"
+# Not sure which files we will really need yet
+
+class SeasonStatistics
+
+  def initialize()
+  end
+end
+
+def winningest_coach
+  # winningest_coach	Name of the Coach with the best win percentage for the season	String
+end
+
+def worst_coach
+  # worst_coach	Name of the Coach with the worst win percentage for the season	String
+end
+
+def most_accurate_team
+  # most_accurate_team	Name of the Team with the best ratio of shots to goals for the season	String
+end
+
+def least_accurate_team
+  # least_accurate_team	Name of the Team with the worst ratio of shots to goals for the season	String
+end
+
+def most_tackles
+  # most_tackles	Name of the Team with the most tackles in the season	String
+end
+
+def fewest_tackles
+  # fewest_tackles	Name of the Team with the fewest tackles in the season	String
+end
