6ddf064091c49aeb3e620810a7820879296f27a0
Logan Riffell <lkriffell@gmail.com>
2020-07-24 20:59:20 -0600

    Add season_statistics_test test outline

---
+++ test/season_statistics_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/season_statistics"
+require "csv"
+
+class SeasonStatisticsTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
