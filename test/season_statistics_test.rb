24438bfb67020f3e174c098858fc6cc418ecdd58
Logan Riffell <lkriffell@gmail.com>
2020-07-25 11:54:38 -0600

    Add test_get_team_name_by_team_id and test_get_coach_by_team_id

--- test/season_statistics_test.rb
+++ test/season_statistics_test.rb
@@ -5,11 +5,48 @@ require "csv"
 
 class SeasonStatisticsTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def setup
+    @season_statistics = SeasonStatistics.new
+  end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_exists
+    assert_instance_of SeasonStatistics, @season_statistics
+  end
 
+  def test_get_coach_by_team_id
+    expected = {
+                3=>"John Tortorella",
+                6=>"Claude Julien",
+                5=>"Dan Bylsma",
+                17=>"Mike Babcock"
+              }
+
+    assert_equal expected, @season_statistics.get_coach_by_team_id
+  end
+
+  def test_get_team_name_by_team_id
+    expected = {
+                1=>"Atlanta United",
+                4=>"Chicago Fire",
+                26=>"FC Cincinnati",
+                14=>"DC United",
+                6=>"FC Dallas",
+                3=>"Houston Dynamo",
+                5=>"Sporting Kansas City",
+                17=>"LA Galaxy",
+                28=>"Los Angeles FC",
+                18=>"Minnesota United FC",
+                23=>"Montreal Impact",
+                16=>"New England Revolution",
+                9=>"New York City FC",
+                8=>"New York Red Bulls",
+                30=>"Orlando City SC",
+                15=>"Portland Timbers",
+                19=>"Philadelphia Union",
+                24=>"Real Salt Lake",
+                27=>"San Jose Earthquakes"
+              }
+
+    assert_equal expected, @season_statistics.get_team_name_by_team_id
+  end
 end
