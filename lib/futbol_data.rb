285a4750f1334214992352f3e1e0e6d0e03a60cd
Priya Power <priyajag72@gmail.com>
2020-07-29 16:45:48 -0600

    Refactor csv class after league refactor

--- lib/futbol_data.rb
+++ lib/futbol_data.rb
@@ -59,6 +59,55 @@ class FutbolData
     @game_teams
   end
 
+  # Used in Numerous
+
+  def collect_game_objects_by_team_id(passed_id)
+    @by_team_id_game_objects = []
+    @all_games.each do |game_object|
+      if game_object["home_team_id"] == passed_id
+        @by_team_id_game_objects << game_object
+      elsif game_object["away_team_id"] == passed_id
+        @by_team_id_game_objects << game_object
+      end
+    end
+  end
+
+  def create_coach_by_team_id(season)
+    @coach_by_team_id = Hash.new{ |hash, key| hash[key] = 0 }
+    @all_game_teams.each do |game_by_team|
+      if game_by_team["game_id"][0..3] == season[0..3]
+        @coach_by_team_id[game_by_team["team_id"]] = game_by_team["head_coach"]
+      end
+    end
+  end
+
+  # Used in League
+  def count_of_teams
+    @all_teams.size
+  end
+
+  def by_id_suite
+    goals_by_id
+    games_by_id
+  end
+
+  def goals_by_id
+    @goals_by_id = Hash.new{ |hash, key| hash[key] = 0 }
+    @all_game_teams.each do |game_team|
+      @goals_by_id[game_team["team_id"]] += game_team["goals"].to_i
+    end
+    @goals_by_id
+  end
+
+  def games_by_id
+    @games_played_by_id = Hash.new{ |hash, key| hash[key] = 0 }
+    @all_game_teams.each do |game_team|
+      @games_played_by_id[game_team["team_id"]] += 1
+    end
+    @games_played_by_id
+  end
+
+  # Helpers for Team
   def team_info(passed_id)
     @team_info_by_id = Hash.new
     @all_teams.each do |team|
@@ -76,34 +125,4 @@ class FutbolData
     @team_info_by_id["abbreviation"] = team["abbreviation"]
     @team_info_by_id["link"] = team["link"]
   end
-
-  def collect_game_objects_by_team_id(passed_id)
-    @by_team_id_game_objects = []
-    @all_games.each do |game_object|
-      if game_object["home_team_id"] == passed_id
-        @by_team_id_game_objects << game_object
-      elsif game_object["away_team_id"] == passed_id
-        @by_team_id_game_objects << game_object
-      end
-    end
-  end
-
-  def count_of_teams
-    @all_teams.size
-  end
-
-  def create_coach_by_team_id(season)
-    @coach_by_team_id = Hash.new{ |hash, key| hash[key] = 0 }
-    @all_game_teams.each do |game_by_team|
-      if game_by_team["game_id"][0..3] == season[0..3]
-        @coach_by_team_id[game_by_team["team_id"]] = game_by_team["head_coach"]
-      end
-    end
-  end
-
-  def by_id_suite
-    goals_by_id
-    games_by_id
-  end
-  
 end
